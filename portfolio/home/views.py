ec9638c5fe751f6920e9102e9091d3444f8cbc1e
Mateusz <mateusz_90@safe-mail.net>
2014-03-11 20:11:30 +0100

    base

--- portfolio/home/views.py
+++ portfolio/home/views.py
@@ -1,1 +1,10 @@
-# Create your views here.
+#-*- coding: utf-8 -*-
+from django.views.generic.base import View
+from django.shortcuts import render
+
+
+class Home(View):
+    template_name = "home.html"
+
+    def get(self, request):
+        return render(request, self.template_name, {})
