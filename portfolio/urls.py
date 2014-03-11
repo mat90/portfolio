d55e7b5cb9b11ba83f16b6fc3caec1b2b8977a44
Mateusz <mateusz_90@safe-mail.net>
2014-03-11 20:15:42 +0100

    base

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,12 +1,9 @@
 from django.conf.urls import patterns, include, url
-
+from home.views import Home
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'portfolio.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', Home.as_view(), name='home'),
     url(r'^admin/', include(admin.site.urls)),
 )
