7bb681b66b21a5b75203ef2d4e69a11eba3bd2b3
Mateusz <mateusz_90@safe-mail.net>
2014-03-13 07:11:19 +0100

    base template

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,9 +1,14 @@
 from django.conf.urls import patterns, include, url
 from apps.home.views import Home
 from django.contrib import admin
+from settings import MEDIA_ROOT
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    (r'^media/(?P<path>.*)$',
+    'django.views.static.serve',
+    {'document_root': MEDIA_ROOT}),
+
     url(r'^$', Home.as_view(), name='home'),
     url(r'^admin/', include(admin.site.urls)),
 )
